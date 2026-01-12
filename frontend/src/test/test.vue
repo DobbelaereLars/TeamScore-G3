b1f02538624c3b9fce247ce603f01c62e96ab11e
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-12 15:20:22 +0100

    feat: enhance InputField component with dynamic props and update test page

--- frontend/src/test/test.vue
+++ frontend/src/test/test.vue
@@ -4,7 +4,11 @@ import InputField from '../components/InputField.vue';
 
 <template>
   <div class="container mt-5">
-    <InputField />
+    <InputField label="Sessienaam" placeholder="Bv. Sportdag 05/01/2026" />
+    <br />
+    <InputField :label="false" placeholder="Geen label" />
+    <br />
+    <InputField placeholder="Default label placeholder" />
   </div>
 </template>
 
