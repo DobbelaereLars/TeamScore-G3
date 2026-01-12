f3ce831b07920b43f5ba03f35153a2d421358bf7
RenzDeh <renz.deheegher@student.howest.be>
2026-01-12 16:15:06 +0100

    feat: toggle component created

---
+++ frontend/src/components/Toggle.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <label class="c-toggle" for="toggle-component">
+    <input id="toggle-component" name="toggle-component" class="c-toggle__input" type="checkbox" />
+    <span class="c-toggle__thumb"></span>
+  </label>
+</template>
+
+<style scoped></style>
--- frontend/src/views/TabletHomeView.vue
+++ frontend/src/views/TabletHomeView.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+import Toggle from "../components/Toggle.vue";
+</script>
 
 <template>
   <div>
     <h1>Tablet Home View</h1>
+    <Toggle />
   </div>
 </template>
 
