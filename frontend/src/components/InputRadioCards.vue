4f0ca2a62deffde30bba2ba7caa10388275bfbad
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-13 11:12:06 +0100

    feat: update InputRadioCards component with improved styling and structure

--- frontend/src/components/InputRadioCards.vue
+++ frontend/src/components/InputRadioCards.vue
@@ -6,12 +6,50 @@ import { Dices } from 'lucide-vue-next';
   <div class="c-input-radio-cards">
     <div class="c-input-radio-cards__wrapper">
       <input
-        class="c-input-radio-cards__input"
+        class="c-input-radio-cards__input u-hide"
         type="radio"
         name="radio-cards"
         id="radio-card-1"
       />
       <label class="c-input-radio-cards__label" for="radio-card-1">
+        <div class="c-input-radio-cards__label__icon">
+          <Dices />
+        </div>
+
+        <div class="c-input-radio-cards__label__tekst">
+          <p class="u-bold">Speltype</p>
+          <span>Beschrijving</span>
+        </div>
+      </label>
+    </div>
+
+    <div class="c-input-radio-cards__wrapper">
+      <input
+        class="c-input-radio-cards__input u-hide"
+        type="radio"
+        name="radio-cards"
+        id="radio-card-2"
+      />
+      <label class="c-input-radio-cards__label" for="radio-card-2">
+        <div class="c-input-radio-cards__label__icon">
+          <Dices />
+        </div>
+
+        <div class="c-input-radio-cards__label__tekst">
+          <p class="u-bold">Speltype</p>
+          <span>Beschrijving</span>
+        </div>
+      </label>
+    </div>
+
+    <div class="c-input-radio-cards__wrapper">
+      <input
+        class="c-input-radio-cards__input u-hide"
+        type="radio"
+        name="radio-cards"
+        id="radio-card-3"
+      />
+      <label class="c-input-radio-cards__label" for="radio-card-3">
         <div class="c-input-radio-cards__label__icon">
           <Dices />
         </div>
