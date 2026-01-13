f0542dde733e33a0be145c8a930b4d29b89afc12
Yarne Diopere <yarne.diopere@student.howest.be>
2026-01-13 11:02:48 +0100

    feat: add ProfileIcon component

---
+++ frontend/src/components/ProfileIcon.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props = defineProps({
+  ProfileName: {
+    type: String,
+    default: 'S1',
+  }});
+</script>
+
+<template>
+  <div class="c-profile__icon">
+    <p>{{ ProfileName }}</p>
+  </div>
+</template>
+
+<style scoped></style>
