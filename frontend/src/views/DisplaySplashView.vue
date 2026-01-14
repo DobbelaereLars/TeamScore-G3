9ecf2a71f4471816bd36fa9b60ff1b3d08cc0090
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-14 15:36:54 +0100

    feat: implement splash screen layout with logo and title

--- frontend/src/views/DisplaySplashView.vue
+++ frontend/src/views/DisplaySplashView.vue
@@ -1,8 +1,9 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1>Display Splash View</h1>
+  <div class="container p-display-splash-view">
+    <img class="p-display-splash-view__logo" src="../assets/logo.webp" alt="Logo">
+    <h1 class="p-display-splash-view__title h3">Wachten op een sessie...</h1>
   </div>
 </template>
 
