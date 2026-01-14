748d39af3eb864fbca11468e9e0bc35e7d2ea1ee
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-14 14:14:49 +0100

    feat: update TabletHomeView with history section and improve layout styles

--- frontend/src/views/TabletHomeView.vue
+++ frontend/src/views/TabletHomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Button from '../components/Button.vue';
-import { Gamepad2 } from 'lucide-vue-next';
+import { Gamepad2, History } from 'lucide-vue-next';
 </script>
 
 <template>
@@ -10,8 +10,8 @@ import { Gamepad2 } from 'lucide-vue-next';
 
       <p class="p-tablet-home-view__subtitle">
         Het digitale scorebord voor teambuildings, sportieve challenges en
-        groepsactiviteiten.<br>Start snel, hou punten live bij en maak elke ronde
-        meteen zichtbaar.
+        groepsactiviteiten.<br />Start snel, hou punten live bij en maak elke
+        ronde meteen zichtbaar.
       </p>
 
       <Button href="/tablet/game/settings" button-tekst="Maak een spel">
@@ -19,6 +19,21 @@ import { Gamepad2 } from 'lucide-vue-next';
           <Gamepad2 :size="18" />
         </template>
       </Button>
+    </div>
+
+    <div class="p-tablet-home-view__body">
+      <div class="p-tablet-home-view__history">
+        <div class="p-tablet-home-view__history__subtitle">
+          <History :size="32"></History>
+          <h2 class="h4">Vorige sessies</h2>
+        </div>
+
+        <div class="p-tablet-home-view__history__cards">
+          <p class="p-tablet-home-view__history__cards__text">
+            Nog geen sessies gestart
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
