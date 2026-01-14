8db11c2216e1297106c54f8f526067d4bcb77f95
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-14 14:04:53 +0100

    feat: enhance TabletHomeView with logo and subtitle, update Button component usage

--- frontend/src/views/TabletHomeView.vue
+++ frontend/src/views/TabletHomeView.vue
@@ -1,8 +1,25 @@
-<script setup></script>
+<script setup>
+import Button from '../components/Button.vue';
+import { Gamepad2 } from 'lucide-vue-next';
+</script>
 
 <template>
-  <div class="tablet-home">
-    <h1>Tablet Home View</h1>
+  <div class="container p-tablet-home-view">
+    <div class="p-tablet-home-view__head">
+      <img class="p-tablet-home-view__logo" src="../assets/logo.webp" alt="" />
+
+      <p class="p-tablet-home-view__subtitle">
+        Het digitale scorebord voor teambuildings, sportieve challenges en
+        groepsactiviteiten.<br>Start snel, hou punten live bij en maak elke ronde
+        meteen zichtbaar.
+      </p>
+
+      <Button href="/tablet/game/settings" button-tekst="Maak een spel">
+        <template #c-btn_icon-left>
+          <Gamepad2 :size="18" />
+        </template>
+      </Button>
+    </div>
   </div>
 </template>
 
