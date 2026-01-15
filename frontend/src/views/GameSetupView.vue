ce092174c20ba1a5faac911e063d1cf899462fff
Lars Dobbelaere <lars.dobbelaere@student.howest.be>
2026-01-15 10:51:47 +0100

    feat: enhance Game Setup view with logo header and layout adjustments

--- frontend/src/views/GameSetupView.vue
+++ frontend/src/views/GameSetupView.vue
@@ -1,8 +1,34 @@
-<script setup></script>
+<script setup>
+import Button from '../components/Button.vue';
+import { ArrowLeft } from 'lucide-vue-next';
+</script>
 
 <template>
-  <div>
-    <h1>Game Setup View</h1>
+  <div class="container p-game-setup-view">
+    <div class="c-logo-header">
+      <img class="c-logo-header__img" src="../assets/logo.webp" alt="Logo" />
+    </div>
+
+    <div class="row">
+      <div class="col-8 offset-2">
+        <div class="p-game-setup-view__settings">
+          <div class="p-game-setup-view__settings__head">
+            <Button href="/tablet" :is-icon-button="true" variant="secondary">
+              <template #c-btn_icon-left>
+                <ArrowLeft :size="18" />
+              </template>
+            </Button>
+
+            <div class="p-game-setup-view__settings__head__title">
+              <h1 class="h4">Spelinstellingen</h1>
+              <p>Verander hier de instellingen van jouw spel</p>
+            </div>
+          </div>
+
+          <div class="p-game-setup-view__settings__body"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
