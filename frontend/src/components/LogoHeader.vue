43c0010ca7366dee14c7a58ae7c3c6d72303e662
RenzDeh <renz.deheegher@student.howest.be>
2026-01-19 09:52:20 +0100

    feat: logo component gemaakt voor makkelijk gebruik

---
+++ frontend/src/components/LogoHeader.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <div class="c-logo-header">
+        <img class="c-logo-header__img" src="../assets/logo.webp" alt="Logo" />
+    </div>
+</template>
+<style scoped></style>
